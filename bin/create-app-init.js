352d3233ac870621470c85543d06695be34a8f21
lizuncong <2900170896@qq.com>
2020-01-20 10:54:44 +0800

    first commit

---
+++ bin/create-app-init.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const download = require('download-git-repo')
+const chalk = require('chalk')
+const path = require('path')
+const exists = require('fs').existsSync
+const generate = require('../lib/generate')
+const rm = require('rimraf').sync
+const home = require('user-home')
+const ora = require('ora')
+const inquirer = require('inquirer')
+const logger = require('../lib/logger')
+
+
+program
+  .usage('<template-name> [project-name]')
+
+
+program.on('--help', () => {
+  console.log('  例子:')
+  console.log()
+  console.log(chalk.gray('    # 根据提供的模版创建项目'))
+  console.log('    $ create-app init webpack my-project')
+  console.log()
+})
+
+// 如果没有指定参数，则默认输出help信息，并终止程序
+// (调用program.help()会终止程序执行)
+function help () {
+  program.parse(process.argv)
+  if (program.args.length < 1) return program.help()
+}
+help()
+
+
+let template = program.args[0] // 模版名称
+const rawName = program.args[1] // 用户输入的项目名称
+const inPlace = !rawName || rawName === '.' // 项目名为空或者为'.'， 则在当前目录下创建项目
+// name项目名称，如果在当前目录下新建项目，则以当前目录名称做为项目名称
+const name = inPlace ? path.relative('../', process.cwd()) : rawName // 项目名称
+// 将生成的项目存到to目录下面
+const to = path.resolve(rawName || '.')
+
+// 存放项目模版文件的路径，将下载的模版存到tmp目录下面
+const tmp = path.join(home, 'project-templates', template)
+
+console.log()
+process.on('exit', () => {
+  console.log()
+})
+
+if (inPlace || exists(to)) {
+  inquirer.prompt([{
+    type: 'confirm',
+    message: inPlace
+      ? '是否在当前目录创建项目?'
+      : '目录已存在，是否继续?',
+    name: 'ok'
+  }]).then(answers => {
+    if (answers.ok) {
+      run()
+    }
+  }).catch(logger.fatal)
+} else {
+  run()
+}
+
+function run () {
+  downloadAndGenerate()
+}
+
+
+function downloadAndGenerate () {
+  const templateRepo = 'lizuncong/' + template
+  const spinner = ora('正在下载模版')
+  spinner.start()
+
+  if (exists(tmp)) rm(tmp)
+
+  // 下载template到指定的tmp本地路径中
+  download(templateRepo, tmp, { clone: false }, err => {
+    spinner.stop()
+    // 如果generate函数没有捕获处理异常，那么download方法会捕获异常并执行过回调函数抛出异常
+    if (err) logger.fatal('下载模版失败 ' + templateRepo + ': ' + err.message.trim())
+
+    generate(name, tmp, to, generateErr => {
+      if (generateErr) logger.fatal(generateErr)
+      console.log()
+      logger.success('项目生成 "%s".', name)
+    })
+  })
+}
---
+++ bin/create-app-list.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const logger = require('../lib/logger')
+const request = require('request')
+const chalk = require('chalk')
+
+console.log()
+process.on('exit', () => {
+  console.log()
+})
+
+request({
+  url: 'https://api.github.com/users/lizuncong/repos',
+  headers: {
+    'User-Agent': 'create-app-cli', // 不能缺少这个字段
+  }
+}, (err, res, body) => {
+  if (err) logger.fatal(err)
+  const requestBody = JSON.parse(body)
+  if (Array.isArray(requestBody)) {
+    console.log('  所有可用的模版:')
+    console.log()
+    requestBody.forEach(repo => {
+      console.log(
+        '  ' + chalk.yellow('★') +
+        '  ' + chalk.blue(repo.name) +
+        ' - ' + repo.description)
+    })
+  } else {
+    console.error(chalk.red(`${requestBody.message}-${requestBody.documentation_url}`))
+  }
+})

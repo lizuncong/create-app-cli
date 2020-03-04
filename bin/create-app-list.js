f62be96ddc131adab19d7e9bf0bb54e2eb9a9f49
lizuncong <2900170896@qq.com>
2020-03-04 16:20:24 +0800

    模版列表

--- bin/create-app-list.js
+++ bin/create-app-list.js
@@ -4,6 +4,7 @@ const { getTemplateList } = require('../lib/util')
 
 getTemplateList().then(res => {
   res.forEach(repo => {
+    console.log('所有可用的模版：\n')
     console.log(
       '  ' + chalk.yellow('★') +
       '  ' + chalk.blue(repo.name) +
